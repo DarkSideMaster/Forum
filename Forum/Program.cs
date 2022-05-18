872115e773debe50ddecf9f88bf3d24c2e27ad94
Andrii Tkachenko <darkpanter@ukr.net>
2022-05-18 09:57:26 +0300

    First commit

--- Forum/Program.cs
+++ Forum/Program.cs
@@ -5,8 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("ForumDBConnection");
+var connectionString = builder.Configuration.GetConnectionString("ForumDBConnection") ?? throw new InvalidOperationException("Connection string 'ForumDBConnection' not found."); ;
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
