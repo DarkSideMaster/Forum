272f5462fe06d91171c49ebec4da74e5da26d108
Andrii Tkachenko <darkpanter@ukr.net>
2022-05-30 16:58:26 +0300

    fix

--- Forum/Data/Interfaces/IForums.cs
+++ Forum/Data/Interfaces/IForums.cs
@@ -10,6 +10,7 @@ namespace Forum.Data
         Task Create(Forums forum);
         Task Delete(int forumId);
         Task UpdateForumDescription(int forumId, string newDescription);
+        Task UpdateForumTitle(int forumId, string newTitle);
         IEnumerable<ApplicationUser> GetActiveUsers(int id);
         bool HasRecentPost(int id);
     }
